35ca513f54d363e410a119753c2444d36a851807
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-06-03 17:50:59 +0530

    Add files via upload

---
+++ DOM-Ids.js
@@ -0,0 +1,10 @@
+let x= document.getElementById("a");
+console.log(x);
+x.innerText="DOM";
+let y=document.getElementById("b");
+y.innerText="Apple";
+let z=document.getElementById("c");
+z.innerText="Ram";
+let w=document.getElementById("d");
+w.innerText="monika";
+
